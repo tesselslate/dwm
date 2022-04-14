0195e97e4c36760aa4ad4da99048df2c1a7ecab5
woofdoggo <woofwoofdoggo@protonmail.com>
2022-04-14 11:13:12 -0400

    make: ubsan debug flags, destination -> ~/.local

--- config.mk
+++ config.mk
@@ -4,7 +4,7 @@ VERSION = 6.3
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX = ~/.local
 MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
@@ -24,7 +24,7 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lXrender
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
+#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 -fsanitize=undefined -lubsan ${INCS} ${CPPFLAGS}
 CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
